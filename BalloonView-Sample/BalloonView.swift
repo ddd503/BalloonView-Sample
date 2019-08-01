7acb910e8fac5fda651dc1d12351b9c0fa348cac
Dai Kawahara <skbsporta7@gmail.com>
2019-08-02 07:44:06 +0900

    add BalloonViewDirectionType and select show balloon derection

--- BalloonView-Sample/BalloonView.swift
+++ BalloonView-Sample/BalloonView.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
+enum BalloonViewDirectionType {
+    case up
+    case under
+    case right
+    case left
+    case upperRight
+    case lowerRight
+    case upperLeft
+    case lowerLeft
+
+    // TODO: 他のtypeの場合も決める
+    var degree: CGFloat {
+        switch self {
+        case .lowerRight:
+            return 315
+        default:
+            return 0
+        }
+    }
+}
+
 final class BalloonView: UIView {
     // 三角部分の幅
     private let triangleBottomLength: CGFloat
     // 三角部分の高さ
     private let triangleHeight: CGFloat
-    // 塗りつぶしの色
+    // 三角形のX軸の中央値
+    private let triangleBottomCenter: CGPoint
+    // 吹き出しの色
     private let color: UIColor
     // 吹き出しの内容が入るView
     private let innerView: UIView
+    // 吹き出すを出す方向
+    private let directionType: BalloonViewDirectionType
 
     /// 吹き出しのイニシャライズ
     ///
@@ -24,24 +49,30 @@ final class BalloonView: UIView {
     ///   - focusPoint: 吹き出しが出る地点(三角形の頂点)
     ///   - contentView: 吹き出しの中に入れたいView（今回は長方形のUILabelを渡しています）
     ///   - color: 吹き出しの色
+    ///   - directionType: 吹き出すを出す方向
     ///   - triangleBottomLength: 三角形部分の幅
     ///   - triangleHeight: 三角形部分の高さ
     init(focusPoint: CGPoint, contentView: UIView,
-         color: UIColor, triangleBottomLength: CGFloat = 25, triangleHeight: CGFloat = 20) {
+         color: UIColor, directionType: BalloonViewDirectionType,
+         triangleBottomLength: CGFloat = 25, triangleHeight: CGFloat = 20) {
         // 各種プロパティに保持
         self.color = color
+        self.directionType = directionType
         self.triangleBottomLength = triangleBottomLength
         self.triangleHeight = triangleHeight
+        // 三角形部分の中央値を取得
+        self.triangleBottomCenter = BalloonView.circumferenceCoordinate(degree: 315, radius: triangleHeight)
         // 中に入れたいViewのサイズを元に親View(三角形部分含む)のサイズを決めます
         let viewSize = CGSize(width: contentView.frame.size.width * 1.2,
-                              height: contentView.frame.size.height * 1.5 + triangleHeight)
-        let frame = CGRect(origin: CGPoint(x: focusPoint.x - viewSize.width / 2,
-                                           y: focusPoint.y - viewSize.height),
-                           size: viewSize)
+                              height: contentView.frame.size.height * 1.5 + triangleBottomCenter.y)
+
+        let frame = CGRect(origin: focusPoint, size: viewSize)
+
         // 吹き出し内容View(三角形部分を含まない)のサイズを決めます
-        self.innerView = UIView(frame: CGRect(origin: .zero,
+        self.innerView = UIView(frame: CGRect(origin: CGPoint(x: 0, y: triangleBottomCenter.y),
                                               size: CGSize(width: viewSize.width,
-                                                           height: viewSize.height - triangleHeight)))
+                                                           height: viewSize.height - triangleBottomCenter.y)))
+
         super.init(frame: frame)
         // 親のsuperViewを透明に（吹き出しのみを見せるため）
         backgroundColor = .clear
@@ -51,7 +82,7 @@ final class BalloonView: UIView {
         innerView.layer.masksToBounds = true
         innerView.layer.cornerRadius = 10
         innerView.addSubview(contentView)
-        contentView.center = innerView.center
+        contentView.center = self.convert(innerView.center, to: innerView)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -71,17 +102,40 @@ final class BalloonView: UIView {
 
         context.setFillColor(color.cgColor)
 
-        // 三角と内容Viewの間に隙間が空いてしまうため+1としている
-        let leftEndPoint = CGPoint(x: rect.size.width / 2 - (triangleBottomLength / 2), y: rect.size.height - (triangleHeight + 1))
-        let rightEndPoint = CGPoint(x: leftEndPoint.x + triangleBottomLength, y: rect.size.height - (triangleHeight + 1))
-        let tipCornerPoint = CGPoint(x: rect.size.width / 2, y: rect.maxY)
+        // 三角形の各頂点を取得
+        let cornerPoints = triangleCornerPoints(type: directionType)
+        let tipCornerPoint = rect.origin
 
         // 開始点を指定
-        context.move(to: leftEndPoint)
+        context.move(to: cornerPoints.left)
         // 移動点から二点以上指定して線を引かないと領域の確保がされずに短形は描画されない
-        context.addLine(to: rightEndPoint)
+        context.addLine(to: cornerPoints.right)
         context.addLine(to: tipCornerPoint)
         // 描画開始
         context.fillPath()
+    }
+
+    private func triangleCornerPoints(type: BalloonViewDirectionType) -> (left: CGPoint, right: CGPoint) {
+        switch type {
+        case .lowerRight:
+            let left = CGPoint(x: (triangleBottomCenter.x - innerView.frame.origin.x) * 0.8, y: innerView.frame.origin.y)
+            let right = CGPoint(x: triangleBottomCenter.x + triangleBottomLength, y: innerView.frame.origin.y)
+            return (left, right)
+        default:
+            return (.zero, .zero) // TODO: 他のtypeの場合も決める
+        }
+    }
+
+    /// 円周上の座標を返す
+    ///
+    /// - Parameters:
+    ///   - degree: 角度
+    ///   - radius: 半径
+    /// - Returns: 円周上の座標時
+    static private func circumferenceCoordinate(degree: Double, radius: CGFloat) -> CGPoint {
+        let θ = Double.pi / Double(-180) * Double(degree)
+        let x = Double(radius) * cos(θ)
+        let y = Double(radius) * sin(θ)
+        return CGPoint(x: x, y: y)
     }
 }
--- BalloonView-Sample/ViewController.swift
+++ BalloonView-Sample/ViewController.swift
@@ -13,16 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak private var blueView: UIView!
     @IBOutlet weak private var greenView: UIView!
     @IBOutlet weak private var yellowView: UIView!
-    
+
     // 赤色Viewのタップ時
     @IBAction func tappedRedView(_ sender: UITapGestureRecognizer) {
         let titleLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
         titleLabel.textAlignment = .center
         titleLabel.text = "こんにちは！"
         titleLabel.sizeToFit()
-        sender.showBalloonView(color: .white, contentView: titleLabel)
+        sender.showBalloonView(color: .white, contentView: titleLabel, directionType: .lowerRight)
     }
-    
+
     // 青色Viewのタップ時
     @IBAction func tappedBlueView(_ sender: UITapGestureRecognizer) {
         // 複数行のラベル
@@ -31,9 +31,9 @@ class ViewController: UIViewController {
         titleLabel.text = "タップ地点から\n吹き出しを出せる\nようにしました！"
         titleLabel.numberOfLines = 0
         titleLabel.sizeToFit()
-        sender.showBalloonView(color: .white, contentView: titleLabel)
+        sender.showBalloonView(color: .white, contentView: titleLabel, directionType: .lowerRight)
     }
-    
+
     // 緑色Viewのタップ時
     @IBAction func tappedGreenView(_ sender: UITapGestureRecognizer) {
         let titleLabel = UILabel(frame: CGRect(origin: .zero, size: .zero))
@@ -41,9 +41,9 @@ class ViewController: UIViewController {
         titleLabel.text = "テキスト以外も\n入れられるよ →"
         titleLabel.numberOfLines = 0
         titleLabel.sizeToFit()
-        sender.showBalloonView(color: .white, contentView: titleLabel)
+        sender.showBalloonView(color: .white, contentView: titleLabel, directionType: .lowerRight)
     }
-    
+
     // 黄色Viewのタップ時
     @IBAction func tappedYellowView(_ sender: UITapGestureRecognizer) {
         let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 40)))
@@ -53,11 +53,11 @@ class ViewController: UIViewController {
         button.setTitleColor(.white, for: .normal)
         button.setTitleColor(UIColor.white.withAlphaComponent(0.5), for: .highlighted)
         button.addTarget(self, action: #selector(removeAllBalloonView(sender:)), for: .touchUpInside)
-        sender.showBalloonView(color: .white, contentView: button)
+        sender.showBalloonView(color: .white, contentView: button, directionType: .lowerRight)
         button.layer.masksToBounds = true
         button.layer.cornerRadius = 5
     }
-    
+
     @objc func removeAllBalloonView(sender: UIButton) {
         let targetViews = [redView, blueView, greenView, yellowView]
         targetViews.forEach { (targetView) in
@@ -80,21 +80,25 @@ private extension UITapGestureRecognizer {
     /// - Parameters:
     ///   - color: 吹き出しの色
     ///   - contentView: 吹き出し内に入れたいView
-    func showBalloonView(color: UIColor, contentView: UIView) {
+    ///   - directionType: 吹き出しを出したい方向
+    func showBalloonView(color: UIColor, contentView: UIView, directionType: BalloonViewDirectionType) {
         guard let tappedView = self.view else { return }
-        
+
         // 吹き出しの表示数はタップしたView内で1つのみとする
         tappedView.subviews.forEach {
             if $0 is BalloonView {
                 $0.removeFromSuperview()
             }
         }
-        
+
         let tapPosition = self.location(in: tappedView)
-        let balloonView = BalloonView(focusPoint: tapPosition, contentView: contentView, color: color)
+        let balloonView = BalloonView(focusPoint: tapPosition,
+                                      contentView: contentView,
+                                      color: color,
+                                      directionType: directionType)
         balloonView.alpha = 0
         tappedView.addSubview(balloonView)
-        
+
         UIView.animate(withDuration: 0.3) {
             balloonView.alpha = 1.0
         }
